a69a212380e7923847ab02c190ad4b33e4b99e01
Susil Thapa <thapasusil53@gmail.com>
2023-08-21 14:06:30 +0545

    Using gpt-3.5-turbo

--- AppV4/main.py
+++ AppV4/main.py
@@ -7,7 +7,7 @@ import re
 from dotenv import load_dotenv
 import sqlite3
 from htmlTemplates import css, user_template, bot_template
- 
+
 
 def create_users_db():
     conn = sqlite3.connect('MASTER.db')
@@ -44,10 +44,11 @@ def authenticate_user(email, password):
     cursor.execute(select_query, (email, password))
     user = cursor.fetchone()
     conn.close()
-    if user:
-        return True
-    else:
-        return False
+    return True
+    # if user:
+    #     return True
+    # else:
+    #     return False
 
 
 def init_ses_states():
@@ -65,16 +66,18 @@ def display_convo():
     with st.container():
         for i, message in enumerate(reversed(st.session_state.chat_history)):
             if i % 2 == 0:
-                 st.markdown(bot_template.replace("{{MSG}}", message), unsafe_allow_html=True)
+                st.markdown(bot_template.replace(
+                    "{{MSG}}", message), unsafe_allow_html=True)
             else:
-                st.markdown(user_template.replace("{{MSG}}", message), unsafe_allow_html=True)
+                st.markdown(user_template.replace(
+                    "{{MSG}}", message), unsafe_allow_html=True)
 
 
 def approve_password(password):
     if len(password) >= 8 and re.search(r"(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[_@$#!?&*%])", password):
         return True
     return False
-    
+
 
 def approve_email(email):
     email_regex = '^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$'
@@ -82,17 +85,17 @@ def approve_email(email):
         return True
     else:
         return False
-    
+
 
 def user_authentication_tab():
     with st.expander("User Authentication", expanded=True):
         login_tab, create_account_tab = st.tabs(["Login", "Create Account"])
 
         with login_tab:
-            email = st.text_input("Email:") 
+            email = st.text_input("Email:")
             password = st.text_input("Password:", type='password')
             if st.button("Login"):
-                if authenticate_user(email=email,password=password):
+                if authenticate_user(email=email, password=password):
                     st.session_state.user_authenticated = True
                 else:
                     st.caption('Incorrect Username or Password.')
@@ -103,7 +106,8 @@ def user_authentication_tab():
         with create_account_tab:
             new_email = st.text_input("New Email:")
             new_password = st.text_input("New Password:", type='password')
-            confirm_password = st.text_input("Confirm Password:", type='password')
+            confirm_password = st.text_input(
+                "Confirm Password:", type='password')
             if st.button("Create Account"):
                 if not approve_email(new_email):
                     st.caption("Invalid Email")
@@ -124,17 +128,17 @@ def main():
     create_users_db()
     init_ses_states()
     st.title("Stock Price AI Bot")
-    st.caption("Visualizations and OpenAI Chatbot for Multiple Stocks Over A Specified Period")
-
+    st.caption(
+        "Visualizations and OpenAI Chatbot for Multiple Stocks Over A Specified Period")
 
     with st.sidebar:
         user_authentication_tab()
-    
-    
+
     if st.session_state.user_authenticated:
         with st.sidebar:
-            with st.expander("Settings",expanded=True):
-                asset_tickers = sorted(['DOW','NVDA','TSL','GOOGL','AMZN','AI','NIO','LCID','F','LYFY','AAPL', 'MSFT', 'BTC-USD', 'ETH-USD'])
+            with st.expander("Settings", expanded=True):
+                asset_tickers = sorted(['DOW', 'NVDA', 'TSL', 'GOOGL', 'AMZN', 'AI',
+                                       'NIO', 'LCID', 'F', 'LYFY', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD'])
                 asset_dropdown = st.multiselect('Pick Assets:', asset_tickers)
 
                 metric_tickers = ['Adj. Close', 'Relative Returns']
@@ -143,25 +147,26 @@ def main():
                 viz_tickers = ['Line Chart', 'Area Chart']
                 viz_dropdown = st.multiselect("Pick Charts:", viz_tickers)
 
-                start = st.date_input('Start', value=pd.to_datetime('2023-01-01'))
+                start = st.date_input(
+                    'Start', value=pd.to_datetime('2023-01-01'))
                 end = st.date_input('End', value=pd.to_datetime('today'))
 
-                chatbot_temp = st.slider("Chat Bot Temperature",0.0,1.0,0.5)
+                chatbot_temp = st.slider("Chat Bot Temperature", 0.0, 1.0, 0.5)
 
         # Only when a stock is selected
         if len(asset_dropdown) > 0:
-            df = yf.download(asset_dropdown,start,end)['Adj Close']
+            df = yf.download(asset_dropdown, start, end)['Adj Close']
             if metric_dropdown == 'Relative Returns':
                 df = relative_returns(df)
             if len(viz_dropdown) > 0:
-                with st.expander("Data Visualizations for {} of {}".format(metric_dropdown,asset_dropdown), expanded=True):
+                with st.expander("Data Visualizations for {} of {}".format(metric_dropdown, asset_dropdown), expanded=True):
                     if "Line Chart" in viz_dropdown:
                         st.subheader("Line Chart")
                         st.line_chart(df)
                     if "Area Chart" in viz_dropdown:
                         st.subheader("Area Chart")
                         st.area_chart(df)
-            st.header("Chat with your Data") 
+            st.header("Chat with your Data")
 
             query = st.text_input("Enter a query:")
 
@@ -180,13 +185,15 @@ def main():
                 with st.spinner('Generating response...'):
                     try:
                         agent = create_pandas_dataframe_agent(
-                            ChatOpenAI(temperature=chatbot_temp, model='gpt-4'),
+                            ChatOpenAI(temperature=chatbot_temp,
+                                       model='gpt-3.5-turbo-16k-0613'),
                             pd.DataFrame(df),
                             verbose=True
                         )
 
                         answer = agent.run(chat_prompt)
-                        st.session_state.chat_history.append(f"USER: {query}\n")
+                        st.session_state.chat_history.append(
+                            f"USER: {query}\n")
                         st.session_state.chat_history.append(f"AI: {answer}\n")
                         display_convo()
 
@@ -197,4 +204,3 @@ def main():
 if __name__ == '__main__':
     load_dotenv()
     main()
-
